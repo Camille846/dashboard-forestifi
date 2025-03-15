023359adb55d5e83e2ca09ef2cc31050116531fc
Camille <camillegomes74@gmail.com>
2025-03-15 14:46:36 -0300

    Feat: update main page construction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import { Suspense } from "react"
-import Dashboard from "@/components/dashboard"
 import Loading from "@/components/ui/loading"
-import localFont from "next/font/local";
+import localFont from "next/font/local"
 
 const BRSonoma = localFont({ src: '../../public/fonts/BRSonoma-Regular.otf' })
 
 export default function Home() {
-  return (
-      <main className={`${BRSonoma.className} min-h-screen `}>
-        <Suspense fallback={<Loading />}>
-          <Dashboard />
-        </Suspense>
-      </main>
-  )
+    return (
+        <main className={`${BRSonoma.className} min-h-screen`}>
+            <Suspense fallback={<Loading />}></Suspense>
+        </main>
+    )
 }
-
