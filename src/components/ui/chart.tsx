a25a8afbbc661cedb3d432fad0e5c7afac46b4c1
Camille <camillegomes74@gmail.com>
2025-03-15 03:22:48 -0300

    Initial commit

---
+++ src/components/ui/chart.tsx
@@ -0,0 +1,17 @@
+export {
+    PieChart,
+    Pie,
+    Sector,
+    Cell,
+    ResponsiveContainer,
+    Legend,
+    Tooltip,
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    BarChart,
+    Bar,
+} from "recharts"
+
